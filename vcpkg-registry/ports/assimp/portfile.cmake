fc78df3642c61907a1bb9e65781284335b213fc4
Lassi Helynranta <lassi@helynranta.com>
2022-01-06 13:00:41 +0200

    Update portfile.cmake

--- vcpkg-registry/ports/assimp/portfile.cmake
+++ vcpkg-registry/ports/assimp/portfile.cmake
@@ -6,8 +6,25 @@ vcpkg_from_github(
     HEAD_REF master
 )
 
-string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" ASSIMP_BUILD_SHARED_LIBS)
+file(REMOVE ${SOURCE_PATH}/cmake-modules/FindZLIB.cmake)
+file(REMOVE ${SOURCE_PATH}/cmake-modules/FindIrrXML.cmake)
+#file(REMOVE_RECURSE ${SOURCE_PATH}/contrib/clipper) # https://github.com/assimp/assimp/issues/788
+file(REMOVE_RECURSE ${SOURCE_PATH}/contrib/poly2tri)
+file(REMOVE_RECURSE ${SOURCE_PATH}/contrib/zlib)
+file(REMOVE_RECURSE ${SOURCE_PATH}/contrib/gtest)
+file(REMOVE_RECURSE ${SOURCE_PATH}/contrib/irrXML)
+file(REMOVE_RECURSE ${SOURCE_PATH}/contrib/rapidjson)
+file(REMOVE_RECURSE ${SOURCE_PATH}/contrib/stb_image)
+file(REMOVE_RECURSE ${SOURCE_PATH}/contrib/zip)
+file(REMOVE_RECURSE ${SOURCE_PATH}/contrib/unzip)
+file(REMOVE_RECURSE ${SOURCE_PATH}/contrib/utf8cpp)
+#file(REMOVE_RECURSE ${SOURCE_PATH}/contrib/Open3DGC)      #TODO
+#file(REMOVE_RECURSE ${SOURCE_PATH}/contrib/openddlparser) #TODO
 
+set(VCPKG_C_FLAGS "${VCPKG_C_FLAGS} -D_CRT_SECURE_NO_WARNINGS")
+set(VCPKG_CXX_FLAGS "${VCPKG_CXX_FLAGS} -D_CRT_SECURE_NO_WARNINGS")
+
+string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" ASSIMP_BUILD_SHARED_LIBS)
 set (ASSIMP_VCPKG_IMPORTER_EXPORTER_FEATURES "")
 foreach(feature ${FEATURES})
     string( FIND ${feature} "-importer" ASSIMP_VCPKG_FEATURE_TEST)
@@ -69,10 +86,21 @@ if(ASSIMP_DBG)
     vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig/assimp.pc" "-lassimp" "-l${ASSIMP_NAME_DBG}")
 endif()
 
-vcpkg_fixup_cmake_targets(CONFIG_PATH "lib/cmake/assimp-5.1" TARGET_PATH "share/assimp")
+vcpkg_fixup_cmake_targets()
 vcpkg_fixup_pkgconfig() # Probably requires more fixing for static builds. See qt5-3d and the config changes below
-
 vcpkg_copy_pdbs()
+
+file(READ ${CURRENT_PACKAGES_DIR}/share/assimp/assimpConfig.cmake ASSIMP_CONFIG)
+file(WRITE ${CURRENT_PACKAGES_DIR}/share/assimp/assimpConfig.cmake "
+include(CMakeFindDependencyMacro)
+find_dependency(ZLIB)
+find_dependency(irrlicht CONFIG)
+find_dependency(polyclipping CONFIG)
+find_dependency(minizip CONFIG)
+find_dependency(kubazip CONFIG)
+find_dependency(poly2tri CONFIG)
+find_dependency(utf8cpp CONFIG)
+${ASSIMP_CONFIG}")
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
